60104b0450339f378f53647555cf641219deacf3
Allam9573 <aallamdaniel@gmail.com>
2024-04-09 18:00:10 -0600

    inicio index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('base')
+@section('content')
+    <div class="container">
+        <div class="card my-auto">
+            <div class="card-header bg-primary text-white">
+                Proximo vuelo
+            </div>
+            <div class="card-body">
+                dqwd
+            </div>
+        </div>
+    </div>
+@endsection
